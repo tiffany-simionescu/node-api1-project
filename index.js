49bcba27a9d45db16a1c96d3eefb4f8d46a94fc7
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-14 00:55:03 -0500

    POST works

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ app.use(express.json());
 
 // Import DB
 let db = require('./data/db');
+let users = require('./data/seeds/users');
 
 // === Required Endpoints === //
 
 // POST - /api/users - adds user
+app.post("/api/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+  } 
+
+  const newUser = {
+    id: Date.now(),
+    name: req.body.name,
+    bio: req.body.bio,
+    created_at: Date.now(),
+    updated_at: Date.now()
+  }
+
+  db.insert(newUser);
+  res.status(201).json(newUser);
+
+  // If there's an error while saving the user:
+  if(!db.push(newUser)) {
+    return res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+  }
+});
 
 // GET - /api/users - all users
 
@@ -19,3 +41,10 @@ let db = require('./data/db');
 // DELETE - /api/users/:id - deletes user
 
 // PUT - /api/users/:id - edits user
+
+const port = 8000;
+const host = "127.0.0.1";
+
+app.listen(port, host, () => {
+  console.log(`API running at http://${host}:${port}`);
+});
