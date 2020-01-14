d659e7ffbdbaa86de41bc3cc5f24a4824c9634e8
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-14 00:30:49 -0500

    Initial Install - added notes for server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Import DB
+let db = require('./data/db');
+
+// === Required Endpoints === //
+
+// POST - /api/users - adds user
+
+// GET - /api/users - all users
+
+// GET - /api/users/:id - specific user
+
+// DELETE - /api/users/:id - deletes user
+
+// PUT - /api/users/:id - edits user
